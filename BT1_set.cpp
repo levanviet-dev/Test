0916c5fe41ad4d37983769fe1ab24b1c1e503b93
levanviet-dev <70868610+levanviet-dev@users.noreply.github.com>
2020-09-12 00:01:58 +0700

    Add files via upload

---
+++ BT1_set.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<vector>
+using namespace std;
+int main()
+{
+	vector<int> v ={1,2,3,4,5,5,6};
+	set<int> s;
+	for(int i=0;i<v.size();++i)
+	{
+		s.insert(v[i]);
+	}
+	cout<<"So phan tu khac nhau la: "<<s.size();
+}
